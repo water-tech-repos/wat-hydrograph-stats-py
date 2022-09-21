622ed8c33236fc735ba190129696bb159333c131
Brendan Barnes <barne856@umn.edu>
2022-09-21 17:55:20 +0000

    remove post create script from dev container

--- .devcontainer/postcreate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget https://github.com/gyanz/pydsstools/raw/master/dist/pydsstools-2.1-cp38-cp38-linux_x86_64.whl && \
-pip install ./pydsstools-2.1-cp38-cp38-linux_x86_64.whl && \
-rm ./pydsstools-2.1-cp38-cp38-linux_x86_64.whl
